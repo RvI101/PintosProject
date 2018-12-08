a369c497cabbdbd75797f01b181ad1cf64f998db
Arvind Ramachandran <arvindrmc7@gmail.com>
2018-12-08 14:07:54 -0500

    fn to check user meme

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -59,29 +59,36 @@ int write(int fd, const void *buf, unsigned size)
 {
 //    printf("%d is fd\n",fd);
   if(!valid_user_range(buf, size))
-      exit(-1);
+    exit(-1);
+
+  if (!is_mapped_memory(buffer, size, false))
+      _exit (-1);
+
+  if (size <= 0)
+      return 0;
+
   if(fd == 1) {
-	putbuf(buf, size);
+	  return putbuf(buf, size);
   }
-  return size;
+  return 0;
 }
 
 static bool is_mapped_memory(const void *vaddr, size_t size, bool to_be_written)
 {
-    if(!valid_user_vaddr(vaddr))
-        return false;
+  if(!valid_user_vaddr(vaddr))
+      return false;
 
-    void *page = pg_round_down (vaddr);
-    while (page < vaddr + size)
-    {
-        uint32_t *pte = lookup_page (thread_current()->pagedir, page, false);
-        if (pte == NULL || *pte == 0)
-            return false;
-        if (!(*pte & PTE_P) || !(*pte & PTE_U))
-            return false;
-        if (to_be_written && !(*pte & PTE_W))
-            return false;
-        page += PGSIZE;
-    }
-    return true;
+  void *page = pg_round_down (vaddr);
+  while (page < vaddr + size)
+  {
+      uint32_t *pte = lookup_page (thread_current()->pagedir, page, false);
+      if (pte == NULL || *pte == 0)
+          return false;
+      if (!(*pte & PTE_P) || !(*pte & PTE_U))
+          return false;
+      if (to_be_written && !(*pte & PTE_W))
+          return false;
+      page += PGSIZE;
+  }
+  return true;
 }
